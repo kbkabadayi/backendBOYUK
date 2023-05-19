f8153bf504df681c64f590aa40e1d011bb4f5893
torunb <utkuboran@windowslive.com>
2023-05-19 18:11:06 +0300

    Delete hospital.py

--- app/hospital.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-hospital = Blueprint('hospital', __name__, url_prefix='/hospital')
-
-@hospital.route('/endpoint2')
-def endpoint2():
-    return jsonify({'message': 'Endpoint 2'})
