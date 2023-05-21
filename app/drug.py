ead8c2b3a2e2fb23e6d6989a214e229a70f46d12
kbkabadayi <kbkabadayi7@gmail.com>
2023-05-21 20:26:47 +0300

    prescription gereken ilac logic

--- app/drug.py
+++ app/drug.py
@@ -53,6 +53,16 @@ def orderDrug():
     connection = get_connection()
     cursor = connection.cursor(MySQLdb.cursors.DictCursor)
     for drug_name, count in drug_to_count.items():
+        cursor.execute("SELECT needs_prescription FROM Drug WHERE name = %s", drug_name)
+        requires = cursor.fetchone()['needs_prescription']
+        if requires == 'YES':
+            cursor.execute("SELECT drug_name FROM Contains WHERE presc_id in (SELECT presc_id FROM Prescribes WHERE patient_TCK = %s)", patient_TCK)
+            drugs_prescribed = cursor.fetchall()
+            if drug_name not in drugs_prescribed:
+                return jsonify({"result": "Order contains a drug patient is not prescribed"})
+
+
+
         cursor.execute("UPDATE HasDrug SET drug_count = drug_count - %s WHERE drug_name = %s AND pharmacy_id = %s", (count, drug_name, pharm_id))
         connection.commit()
 
--- app/prescription.py
+++ app/prescription.py
@@ -5,22 +5,6 @@ from datetime import datetime
 
 prescription = Blueprint('prescription', __name__, url_prefix='/prescription')
 
-@prescription.route('/addPresc', methods = ['GET', 'POST'])
-def add():
-    data = request.json
-
-    presc_id = data['presc_id']
-    date = datetime.now()
-
-    connection = get_connection()
-    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-
-    cursor.execute("INSERT INTO Prescription VALUES(%s, %s)", (presc_id, date))
-
-    connection.commit()
-
-    return "Prescription is added successfully"
-
 @prescription.route('/prescribe', methods = ['GET', 'POST'])
 def prescribe():
     data = request.json
@@ -29,15 +13,19 @@ def prescribe():
     patient_TCK = data['patient_TCK']
     presc_id = data['presc_id']
     drugs = data['drugs']
+    date = datetime.now()
 
     connection = get_connection()
     cursor = connection.cursor(MySQLdb.cursors.DictCursor)
 
     cursor.execute("INSERT INTO Prescribes VALUES(%s, %s, %s)", (doctor_TCK, patient_TCK, presc_id))
+    connection.commit()
+    cursor.execute("INSERT INTO Prescription VALUES(%s, %s)", (presc_id, date))
+    connection.commit()
 
     for name in drugs:
         cursor.execute("INSERT INTO Contains VALUES(%s, %s)", (presc_id, name))
+        connection.commit()
 
-    connection.commit()
-
+    
     return "Prescription is written to patient successfully"
